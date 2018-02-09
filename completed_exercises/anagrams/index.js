b1588f044726f68c1a1a6d8ea257d0575bd30445
Patrick Vanegas <pvanny1124@gmail.com>
2018-02-09 14:03:19 -0500

    Update index.js

--- completed_exercises/anagrams/index.js
+++ completed_exercises/anagrams/index.js
@@ -8,44 +8,42 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {
-  return cleanString(stringA) === cleanString(stringB);
+//Solution 1
+/*function anagrams(stringA, stringB) {
+    //use the replace() method and RegExp
+    //ex: word.replace(/[^\w]/g, "") -> second param replaces exclamation points, 
+    //and any other special character with an empty string
+    
+    var charMapA = buildCharMap(stringA);
+    var charMapB = buildCharMap(stringB);
+    
+    //Check if both strings have different lengths
+    if(Object.keys(charMapA).length !== Object.keys(charMapB).length) return false;
+    
+    //Check if maps have the same values in their keys
+    for(let letter in charMapA){
+        if(charMapA[letter] !== charMapB[letter]) return false;
+    }
+    
+    //if we reach this point, the two strings must be the same
+    return true;
+}*/
+
+//Solution 2:  using sort() ; a much cleaner method!
+function anagrams(stringA, stringB){
+    return cleanString(stringA) === cleanString(stringB);
 }
 
-function cleanString(str) {
-  return str
-    .replace(/[^\w]/g, '')
-    .toLowerCase()
-    .split('')
-    .sort()
-    .join('');
+function cleanString(str){
+    //sort() is a method for Arrays 
+    return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
 }
 
+//Helper function to dry up code
+/*function buildCharMap(str){
+        const charMap = {};
+        for(let char of str.replace(/[^\w]/g, '').toLowerCase()) charMap[char] ? charMap[char]++ : charMap[char] = 1;
+        return charMap;
+}
+*/
 module.exports = anagrams;
-
-// function anagrams(stringA, stringB) {
-//   const aCharMap = buildCharMap(stringA);
-//   const bCharMap = buildCharMap(stringB);
-//
-//   if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
-//     return false;
-//   }
-//
-//   for (let char in aCharMap) {
-//     if (aCharMap[char] !== bCharMap[char]) {
-//       return false;
-//     }
-//   }
-//
-//   return true;
-// }
-//
-// function buildCharMap(str) {
-//   const charMap = {};
-//
-//   for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
-//     charMap[char] = charMap[char] + 1 || 1;
-//   }
-//
-//   return charMap;
-// }
